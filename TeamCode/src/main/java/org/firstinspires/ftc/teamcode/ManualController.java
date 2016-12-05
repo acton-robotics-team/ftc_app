5c7374f5573a44be68a12d1f11f9dd2a77efffeb
Steven Wood <stevenator21@gmail.com>
2016-12-05 16:17:35 -0500

    I love catz in my ear

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualController.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by Steven on 12/5/2016.
+ */
+
+@TeleOp(name = "Activate scooper", group = "Always on")
+public class ManualController extends OpMode {
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void start() {
+        super.start();
+    }
+
+
+    @Override
+    public void loop() {
+    }
+}
