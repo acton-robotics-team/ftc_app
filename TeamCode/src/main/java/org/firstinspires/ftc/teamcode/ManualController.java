0978414963fead08c60b30ec5e16bbd8bfc12d9e
Kevin Liu <kevin@potatofrom.space>
2016-12-11 16:27:17 -0500

    Add tank-style manual control?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualController.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
  * Created by Steven on 12/5/2016.
@@ -10,11 +11,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "Activate scooper", group = "Always on")
 public class ManualController extends OpMode {
-
+    private DcMotor leftDriveMotor = null;
+    private DcMotor rightDriveMotor = null;
 
     @Override
     public void init() {
-
+        leftDriveMotor = hardwareMap.dcMotor.get(Hardware.ID_LEFT_DRIVE_MOTOR);
+        rightDriveMotor = hardwareMap.dcMotor.get(Hardware.ID_RIGHT_DRIVE_MOTOR);
     }
 
     @Override
@@ -25,5 +28,10 @@ public class ManualController extends OpMode {
 
     @Override
     public void loop() {
+        // Set drive motor levels
+        leftDriveMotor.setPower(gamepad1.left_stick_y);
+        rightDriveMotor.setPower(gamepad1.right_stick_y);
+
+        
     }
 }
