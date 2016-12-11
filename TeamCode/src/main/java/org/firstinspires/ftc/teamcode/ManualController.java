6b72b33ba2968379b53ddfc42f3e2e349a49c20d
Kevin Liu <kevin@potatofrom.space>
2016-12-11 16:54:07 -0500

    Add servo scanning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ManualController.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by Steven on 12/5/2016.
@@ -13,11 +14,16 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class ManualController extends OpMode {
     private DcMotor leftDriveMotor = null;
     private DcMotor rightDriveMotor = null;
+    private DcMotor launcherMotor = null;
+    private Servo scooperServo = null;
+    private boolean isLaunching = false;
 
     @Override
     public void init() {
         leftDriveMotor = hardwareMap.dcMotor.get(Hardware.ID_LEFT_DRIVE_MOTOR);
         rightDriveMotor = hardwareMap.dcMotor.get(Hardware.ID_RIGHT_DRIVE_MOTOR);
+        launcherMotor = hardwareMap.dcMotor.get(Hardware.ID_LAUNCHER_MOTOR);
+        scooperServo = hardwareMap.servo.get(Hardware.ID_SCOOPER_SERVO);
     }
 
     @Override
@@ -32,6 +38,22 @@ public class ManualController extends OpMode {
         leftDriveMotor.setPower(gamepad1.left_stick_y);
         rightDriveMotor.setPower(gamepad1.right_stick_y);
 
-        
+        if (gamepad2.right_bumper && gamepad2.left_bumper) {
+            // Firing sequence
+            telemetry.addData("Status", "PREPARE FOR DEATH");
+            telemetry.update();
+            isLaunching = true;
+
+            launcherMotor.setPower(1.0); // FULL SPEED AHEAD
+
+            // Scan servo
+            if (scooperServo.getPosition() == Hardware.POS_SCOOPER_SERVO_DOWN) {
+                scooperServo.setPosition(Hardware.POS_SCOOPER_SERVO_UP);
+            } else if (scooperServo.getPosition() == Hardware.POS_SCOOPER_SERVO_UP) {
+                scooperServo.setPosition(Hardware.POS_SCOOPER_SERVO_DOWN);
+            }
+        } else {
+            launcherMotor.setPower(0.0);
+        }
     }
 }
