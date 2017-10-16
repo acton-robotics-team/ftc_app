8e6c6fc9d0f93d0fd1e5724821d20426100b49d7
Kevin Liu <kevin@potatofrom.space>
2017-10-16 14:59:25 -0400

    Remove unused methods from Manual

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Manual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Manual.java
@@ -13,29 +13,12 @@ import java.util.concurrent.TimeUnit;
 
 @TeleOp(name = "Manual program")
 public class Manual extends LinearOpMode {
-    private ElapsedTime runtime = new ElapsedTime();
-    private String logText = "";
-    private void log(String text) {
-        String str = "[" + runtime.time(TimeUnit.SECONDS) + "] " + text;
-        logText = str + logText;
-        telemetry.addLine(logText);
-        telemetry.update();
-    }
-
-    private void sleep() throws OpModeStoppedException {
-        if (!opModeIsActive())
-            throw new OpModeStoppedException();
-        else
-            idle();
-    }
-
     @Override
     public void runOpMode() {
         Hardware hw = new Hardware(hardwareMap);
 
         // wait for the start button to be pressed.
         waitForStart();
-        runtime.reset();
 
         while (opModeIsActive()) {
             hw.rightDriveMotor.setPower(gamepad1.right_stick_y);
