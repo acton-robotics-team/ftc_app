d011b3eea225b46d76e1a27956eb2613ce6099ad
Dylan Kim <s.dylan.kim@gmail.com>
2017-10-23 15:56:59 -0400

    lifter motor stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Manual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Manual.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
  * Created by nitro on 10/9/2017.
@@ -31,23 +32,24 @@ public class Manual extends LinearOpMode {
             hw.leftGrabberServo.setPosition(limit(gamepad2.left_trigger, 0.5, 1.0));
             hw.rightGrabberServo.setPosition(limit(gamepad2.right_trigger, 0.5, 1.0));
 
-            if (!hw.lifterMotor.isBusy()) {
-                int offset;
-                if (gamepad2.right_stick_y > 0.1) {
-                    // Actually pushing down -- positive offset = move DOWN
-                    hw.lifterMotor.setPower(1);
-                    hw.lifterMotor.setTargetPosition(hw.lifterMotor.getCurrentPosition() + 200);
-                } else if (gamepad2.right_stick_y < -0.1) {
-                    // Actually pushing up -- negative offset = move UP
-                    hw.lifterMotor.setPower(-1);
-                    hw.lifterMotor.setTargetPosition(hw.lifterMotor.getCurrentPosition() - 200);
-                }
-                /*hw.lifterMotor.setTargetPosition(limit(
-                        hw.lifterMotor.getCurrentPosition() + offset,
-                        0, // Do not go below the initial position.
-                        (int) (4.5 * Hardware.TETRIX_TICKS_PER_REVOLUTION))); // Do not go more than 4.5 revolutions*/
+            int offset;
+            int position = hw.lifterMotor.getCurrentPosition();
+            telemetry.addData("Encoder Position", position);
+            hw.lifterMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            if (gamepad2.right_stick_y > 0.1 && position < 0) {
+                // Actually pushing down -- positive offset = move DOWN
+                hw.lifterMotor.setPower(1);
+            } else if (gamepad2.right_stick_y < -0.1 && position > -4500) {
+                // Actually pushing up -- negative offset = move UP
+                hw.lifterMotor.setPower(-1);
             }
-
+            else{
+                hw.lifterMotor.setPower(0);
+            }
+            /*hw.lifterMotor.setTargetPosition(limit(
+                    hw.lifterMotor.getCurrentPosition() + offset,
+                    0, // Do not go below the initial position.
+                    (int) (4.5 * Hardware.TETRIX_TICKS_PER_REVOLUTION))); // Do not go more than 4.5 revolutions*/
             idle();
         }
     }
