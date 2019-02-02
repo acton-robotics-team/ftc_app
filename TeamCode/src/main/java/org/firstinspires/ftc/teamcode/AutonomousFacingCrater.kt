6e8aaab01e88bb4b011c970307923d333bf16add
Kevin Liu <kevin@potatofrom.space>
2019-02-01 19:05:01 -0500

    Switch autonomouses to use WithArm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFacingCrater.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFacingCrater.kt
@@ -3,6 +3,6 @@ package org.firstinspires.ftc.teamcode
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 
 @Autonomous(name = "Facing crater")
-class AutonomousFacingCrater : BaseAutonomous() {
+class AutonomousFacingCrater : BaseAutonomousWithArm() {
     override val startLocation: AutonomousStartLocation = AutonomousStartLocation.FACING_CRATER
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFacingDepot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFacingDepot.kt
@@ -3,6 +3,6 @@ package org.firstinspires.ftc.teamcode
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 
 @Autonomous(name = "Facing depot")
-class AutonomousFacingDepot : BaseAutonomous(){
+class AutonomousFacingDepot : BaseAutonomousWithArm() {
     override val startLocation: AutonomousStartLocation = AutonomousStartLocation.FACING_DEPOT
 }
